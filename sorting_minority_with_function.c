39e0240db8bce89efefabb88838bc6467dc0aa5e
Licoo <46717842+ElectroSciences@users.noreply.github.com>
2021-01-31 19:53:15 +0100

    Update sorting_minority_with_function.c

--- sorting_minority_with_function.c
+++ sorting_minority_with_function.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 
-#define NB_STUDENT 25
+#define NB_STUDENT 26
 #define NB_JOB 7
 
 #define CM 0
@@ -16,16 +16,16 @@
 
 
 
-int arr[NB_JOB][NB_STUDENT] = {{0,0,0,1,1,0,0,0,0,1,1,2,0,0,1,1,1,1,0,0,0,0,1,0,1},   		  // CM
-			       {1,1,0,1,0,0,0,1,0,0,0,0,0,0,0,1,0,1,1,1,1,1,1,0,1},               // P  
-			       {0,0,1,0,0,0,1,0,1,1,1,1,0,0,1,1,2,0,1,0,0,0,1,1,0},               // SC     
-			       {0,1,0,1,0,0,1,0,1,0,1,1,0,0,1,0,0,0,1,0,1,1,0,1,1},               // S            
-			       {0,1,1,0,1,0,1,1,0,0,0,0,0,1,1,1,1,0,1,0,1,0,1,1,1},               // SM  
-			       {0,1,0,1,0,0,1,1,0,0,0,0,1,1,0,1,1,2,0,0,1,0,0,0,0},               // PC  
-			       {0,0,0,0,0,2,0,1,0,1,0,1,1,1,0,0,0,1,0,1,0,0,1,0,1} };             // L  
+int arr[NB_JOB][NB_STUDENT] = {{0,0,0,1,1,0,0,0,0,1,1,2,0,0,1,1,1,1,0,0,0,0,1,0,2,0},   // CM
+			       {1,1,0,1,0,1,1,1,0,0,0,1,0,0,0,1,0,1,1,1,1,1,1,0,1,0},               // P
+			       {0,1,1,0,0,0,1,0,1,1,1,1,0,0,1,1,2,0,1,0,0,0,1,1,1,0},               // SC
+			       {0,1,0,1,0,0,1,0,1,1,1,1,0,0,1,0,0,0,1,0,1,1,0,1,1,1},               // S
+			       {1,1,1,0,1,0,1,1,0,0,0,0,0,1,1,1,1,0,1,1,1,0,1,1,1,0},               // SM
+			       {0,1,1,1,0,0,1,1,0,0,0,0,1,1,1,1,1,2,0,0,1,0,0,0,0,0},               // PC
+			       {0,0,0,1,0,2,0,1,0,1,0,1,1,1,0,0,0,1,0,1,0,1,1,0,1,1} };             // L
 
 void fonction_tri (int persCho[NB_JOB], int arr[][NB_STUDENT])
-{  
+{
   int j, i, k;
     for(i=0; i<NB_JOB; i++)
       {
@@ -46,7 +46,7 @@ void fonction_tri (int persCho[NB_JOB], int arr[][NB_STUDENT])
 		  lazy_student[index++]=j;
 	      }
 	persCho[NB_JOB+i]=lazy_student[rand()%index];
-	
+
       }
 
 }
@@ -54,15 +54,13 @@ void fonction_tri (int persCho[NB_JOB], int arr[][NB_STUDENT])
 
 int main()
 {
-  int persCho[NB_JOB*2]={1,2,3,4,5,6,7};
-    
+  int persCho[NB_JOB*2]={4,5,8,9,12,16,21};
+
   fonction_tri(persCho, arr);
-  
+
   int i;
   for(i=0; i<NB_JOB; i++)
-    printf("job %d => student %d\n", i, persCho[NB_JOB+i]); 
+    printf("job %d => student %d\n", i, persCho[NB_JOB+i]);
 
   return 0;
 }
-
-
